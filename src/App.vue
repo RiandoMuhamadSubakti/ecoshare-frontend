944a7c6012c4c797a0a4689ebff13a8881795d79
RiandoMuhamadSubakti <411231189@mahasiswa.undira.ac.id>
2026-05-25 15:52:33 +0700

    feat: tambah komponen navbar, router, dan halaman dashboard

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<!-- ============================================
+     APP.VUE - Komponen Root
+     Dibuat oleh: Riando Muhamad Subakti
+     Universitas Dian Nusantara
+============================================ -->
+
+<template>
+  <div id="app">
+    <!-- Navbar tampil kalau sudah login -->
+    <NavBar v-if="isLoggedIn" />
+    <RouterView />
+    <div class="watermark">Dibuat oleh: Riando Muhamad Subakti | Universitas Dian Nusantara</div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+
+const isLoggedIn = computed(() => !!localStorage.getItem('eco_token'))
+</script>
